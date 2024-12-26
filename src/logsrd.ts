d7a26609e40b72589d39d63731eecc50fc3a1027
Ersun Warncke <ersun.warncke@gmail.com>
2024-12-25 22:48:19 -0800

    add size checks on all branches of post

--- src/logsrd.ts
+++ src/logsrd.ts
@@ -91,6 +91,10 @@ function readPost(res: HttpResponse, cb: (data: Uint8Array) => void, err: () => 
         let chunk = Buffer.from(ab)
         if (isLast) {
             if (buffer) {
+                if (buffer.length + chunk.length > MAX_ENTRY_SIZE) {
+                    // hard stop connection - no response
+                    res.close()
+                }
                 // copy here because buffer may become detached - this sucks but can
                 // be optimized later
                 const u8 = new Uint8Array(buffer.length + chunk.length)
@@ -98,6 +102,10 @@ function readPost(res: HttpResponse, cb: (data: Uint8Array) => void, err: () => 
                 u8.set(chunk, buffer.length)
                 cb(u8)
             } else {
+                if (chunk.length > MAX_ENTRY_SIZE) {
+                    // hard stop connection - no response
+                    res.close()
+                }
                 const u8 = new Uint8Array(chunk.length)
                 u8.set(chunk)
                 cb(u8)
